738091e8fd4a9c3dc9614bfa0a382ca09c41706a
Elan <elanelango@gmail.com>
2016-03-13 18:38:08 -0700

    After several attempts, a working implementation of pushEntryToFriends() method!

--- cloud/main.js
+++ cloud/main.js
@@ -40,3 +40,72 @@ Parse.Cloud.define('pushChannelTest', function(request, response) {
 
   response.success('success');
 });
+
+Parse.Cloud.define('pushEntryToFriends', function(request, response) {
+  var params = request.params;
+  var user = request.user;
+
+  var customData = params.customData;
+  var launch = params.launch;
+  var broadcast = params.broadcast;
+
+  var username = params.customData;
+  var userQuery = new Parse.Query(Parse.User);
+  userQuery.equalTo("username", username);
+
+  userQuery.find().then(function (results) {
+    console.log("Retrieved user..");
+    return results[0];
+
+  }).then(function(userObj) {
+    console.log("user type: ", Object.prototype.toString.call(userObj));
+    var friendsRelation = userObj.relation("friends");
+    var friendsQuery = friendsRelation.query();
+    return friendsQuery.find();
+
+  }).then(function(friends) {
+    console.log("Retrieved friends..");
+    var subQueries = [];
+    if (friends.length == 0) {
+      return Parse.Promise.as(1);
+    }
+
+    for (var friend of friends) {
+      var query = new Parse.Query(Parse.Installation);
+      query.equalTo("user", friend);
+      subQueries.push(query);
+    }
+
+    var pushQuery = new Parse.Query(Parse.Installation)
+    pushQuery._orQuery(subQueries);
+    pushQuery.equalTo("deviceType", "android");
+
+    console.log("Constructed push query!");
+
+    var payload = {};
+
+    if (customData) {
+      payload.customdata = customData;
+    }
+    else if (launch) {
+      payload.launch = launch;
+    }
+    else if (broadcast) {
+      payload.broadcast = broadcast;
+    }
+
+    return Parse.Push.send({
+      where: pushQuery,      // for sending to a specific channel
+      data: payload,
+    }, {useMasterKey: true});
+
+  }).then( function() {
+    console.log("#### PUSH OK");
+
+  }, function (error) {
+    console.log("Error while promises: ", error);
+    console.log("#### PUSH ERROR" + error.message);
+  });
+
+  response.success('success');
+});
